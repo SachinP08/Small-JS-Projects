b9a978b618f3a800a7abeaf6f439fc9a6bb547e6
Sachin Parihar <pariharsachin397@gmail.com>
2025-07-01 22:20:12 +0530

    Delete Functionality Added

--- To-Do-App/first.js
+++ To-Do-App/first.js
@@ -1,24 +1,50 @@
 const taskInput = document.getElementById("Task-Input");
 const addTaskBtn = document.getElementById("Add-Btn");
 const taskList = document.getElementById("Task-List");
+
 let Task = [];
 
-function addTask() {
-  if (taskInput.value.trim() === "") return;
+function generateID() {
+  return Date.now().toString();
+}
 
-  Task.push(taskInput.value);
+function deleteTask(el) {
+  const taskID = el.target.dataset.id;
+
+  Task = Task.filter((task) => task.id !== taskID);
 
   localStorage.setItem("Tasks", JSON.stringify(Task));
 
+  el.target.parentElement.remove();
+}
+
+function createTaskElement(taskObj) {
   const newTask = document.createElement("li");
-  newTask.textContent = taskInput.value + "";
+  newTask.textContent = taskObj.text + "";
 
   const deleteBtn = document.createElement("i");
-  deleteBtn.className = "ri-delete-bin-line";
-  deleteBtn.id = "Delete-Btn";
+  deleteBtn.className = "ri-delete-bin-line Delete-Btn";
+  deleteBtn.setAttribute("data-id", taskObj.id);
+  deleteBtn.addEventListener("click", deleteTask);
 
   newTask.appendChild(deleteBtn);
-  taskList.appendChild(newTask);
+  return newTask;
+}
+
+function addTask() {
+  const inputValue = taskInput.value.trim();
+  if (inputValue === "") return;
+
+  let taskObj = {
+    id: generateID(),
+    text: inputValue,
+  };
+
+  Task.push(taskObj);
+  localStorage.setItem("Tasks", JSON.stringify(Task));
+
+  const taskElement = createTaskElement(taskObj);
+  taskList.appendChild(taskElement);
 
   taskInput.value = "";
 }
@@ -31,11 +57,12 @@ function onPageReload() {
 
     for (let i = 0; i < allTask.length; i++) {
       const newTask = document.createElement("li");
-      newTask.textContent = allTask[i] + "";
+      newTask.textContent = allTask[i].text + "";
 
       const deleteBtn = document.createElement("i");
-      deleteBtn.className = "ri-delete-bin-line";
-      deleteBtn.id = "Delete-Btn";
+      deleteBtn.className = "ri-delete-bin-line Delete-Btn";
+      deleteBtn.setAttribute("data-id", allTask[i].id);
+      deleteBtn.addEventListener("click", deleteTask);
 
       newTask.appendChild(deleteBtn);
       taskList.appendChild(newTask);
